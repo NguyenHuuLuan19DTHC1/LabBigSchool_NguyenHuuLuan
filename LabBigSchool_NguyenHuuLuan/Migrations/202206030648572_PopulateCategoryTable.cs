1cf6141a9cb22f2a0fbf8a19270b04ecf5480fb8
nglua <nglua@LUAN>
2022-06-03 15:47:46 +0700

    Add project files.

---
+++ LabBigSchool_NguyenHuuLuan/Migrations/202206030648572_PopulateCategoryTable.cs
@@ -0,0 +1,37 @@
+﻿namespace LabBigSchool_NguyenHuuLuan.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Courses", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "Category_Id" });
+            DropColumn("dbo.Courses", "CategoryId");
+            RenameColumn(table: "dbo.Courses", name: "Category_Id", newName: "CategoryId");
+            AddColumn("dbo.Courses", "Place", c => c.String(nullable: false, maxLength: 255));
+            AlterColumn("dbo.Courses", "CategoryId", c => c.Byte(nullable: false));
+            AlterColumn("dbo.Courses", "CategoryId", c => c.Byte(nullable: false));
+            CreateIndex("dbo.Courses", "CategoryId");
+            AddForeignKey("dbo.Courses", "CategoryId", "dbo.Categories", "Id", cascadeDelete: true);
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1,'Development')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2,'Business')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3,'Maketing')");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            AlterColumn("dbo.Courses", "CategoryId", c => c.Byte());
+            AlterColumn("dbo.Courses", "CategoryId", c => c.Binary(nullable: false));
+            DropColumn("dbo.Courses", "Place");
+            RenameColumn(table: "dbo.Courses", name: "CategoryId", newName: "Category_Id");
+            AddColumn("dbo.Courses", "CategoryId", c => c.Binary(nullable: false));
+            CreateIndex("dbo.Courses", "Category_Id");
+            AddForeignKey("dbo.Courses", "Category_Id", "dbo.Categories", "Id");
+        }
+    }
+}
---
+++ LabBigSchool_NguyenHuuLuan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabBigSchool_NguyenHuuLuan.Startup))]
+namespace LabBigSchool_NguyenHuuLuan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
